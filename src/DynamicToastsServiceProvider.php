804717830f63d62ba613607e1d496f47d015c3e3
mrjokermr <46922309+mrjokermr@users.noreply.github.com>
2025-09-05 15:14:09 +0200

    Make config publishable
    Update DynamicToastsServiceProvider.php

--- src/DynamicToastsServiceProvider.php
+++ src/DynamicToastsServiceProvider.php
@@ -8,13 +8,20 @@ use Mrjokermr\DynamicToasts\Livewire\DisplayDynamicToasts;
 
 class DynamicToastsServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(): void
     {
-        $this->loadViewsFrom(__DIR__.'/Resources/Views/', 'dynamic-toasts');
+        $this->loadViewsFrom(__DIR__ . '/Resources/Views', 'dynamic-toasts');
         Livewire::component('dynamic-toasts', DisplayDynamicToasts::class);
 
+        $this->publishes([
+            __DIR__ . '/Config/dynamic-toasts.php' => config_path('dynamic-toasts.php'),
+        ], 'dynamic-toasts-config');
+    }
+
+    public function register(): void
+    {
         $this->mergeConfigFrom(
-            __DIR__ . '/Config/dynamic-toasts.php',
+            __DIR__ . '/Config/dynamic-toasts.php', // adjust if your path differs
             'dynamic-toasts'
         );
     }
