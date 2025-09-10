bcd9a10d53eb72ca24df1087c37ec44a25e6e737
mrjokermr <46922309+mrjokermr@users.noreply.github.com>
2025-09-10 14:37:27 +0200

    Improved toast message width styling
    Update display-dynamic-toasts.blade.php

--- src/Resources/Views/Livewire/display-dynamic-toasts.blade.php
+++ src/Resources/Views/Livewire/display-dynamic-toasts.blade.php
@@ -1,7 +1,7 @@
 <div wire:poll.500ms="removeExpiredMessages"
      style="pointer-events:none; position:fixed; top:0; right:0; bottom:0; left:0; z-index:100; display:flex; justify-content:flex-end; flex-direction:column;">
     <div style="height:5rem; width:100%; display:flex; justify-content:flex-end;">
-        <div style="width:22rem; padding:0.75rem; display:flex; flex-direction:column-reverse; row-gap:0.5rem;">
+        <div style="width:auto; max-width:22rem; padding:0.75rem; display:flex; flex-direction:column-reverse; row-gap:0.5rem;">
             @php /** @var \Mrjokermr\DynamicToasts\Classes\ToastMessage $message */ @endphp
             @foreach($messages as $message)
                 @php $styleClass = $message->getStyleClass(); @endphp
@@ -9,7 +9,7 @@
                     @if (isset($styleClass))
                         class="{!! $styleClass !!}"
                     @else
-                        style="pointer-events:auto; width:auto; max-width:22rem; font-size:0.875rem; padding:0.5rem; border-radius:0.375rem; box-shadow:0 1px 2px 0 rgb(0 0 0 / 0.05); background-color: {{ $message->getBackgroundHexColor() }};">
+                        style="pointer-events:auto; width:auto; font-size:0.875rem; padding:0.5rem; border-radius:0.375rem; box-shadow:0 1px 2px 0 rgb(0 0 0 / 0.05); background-color: {{ $message->getBackgroundHexColor() }};">
                     @endif
                     <div style="color: {{ $message->getTextHexColor() }}; position: relative; display: flex; column-gap: 0.5rem;">
                         @if ($message->showIcon)
